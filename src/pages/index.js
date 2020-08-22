f23dbc1c12f4594e66c0747d3b657c7327a66f21
Nathan Bradshaw <nathansbradshaw@gmail.com>
2020-08-22 14:51:56 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,50 +11,52 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <section className="hero is-light is-bol is-fullheight" >
-      <div className="padding-top-4em " id="tv">
-        <div className="section">
-          <div className="container">
-            <p className="giant-text retro-font retro-text-shadow has-text-centered">Select your character</p>
-          </div>
-        </div>
-        <div className="section">
-          <div className="container">
-            <div className="tile is-ancestor">
-
-              <div className="tile is-parent mx-6">
-                <Link to="projects" className="tile is-child diagonal-box turtle-color-r">
-                  <div className="turtle-text has-text-centered">
-                    <p className="giext retro-font has-text-centered">Project</p>
-                  </div>
-                  <div className="turtle">
-                    <img src="assets/projects.png"></img>
-                  </div>
-                </Link>
-              </div>
-
-              <div className="tile is-parent mx-6">
-                <Link to="about" className="tile is-child diagonal-box turtle-color-y">
-                  <div className="turtle-text has-text-centered">
-                    <p className="giext retro-font has-text-centered">About zack</p>
-                  </div>
-                  <div className="turtle">
-                    <img src="assets/about.png"></img>
-                  </div>
-                </Link>
-              </div>
-
-              <div className="tile is-parent mx-6">
-                <Link to="gallery" className="tile is-child diagonal-box turtle-color-b">
-                  <div className="turtle-text has-text-centered">
-                    <p className="giext retro-font has-text-centered">Gallery</p>
-                  </div>
-                  <div className="turtle">
-                    <img src="assets/artgallery.png"></img>
-                  </div>
-                </Link>
-              </div>
+      <div className="hero-body" id="tv">
+        <div className="padding-top-4em " >
+          <div className="section">
+            <div className="container">
+              <p className="giant-text retro-font retro-text-shadow has-text-centered">Select your character</p>
             </div>
+          </div>
+          <div className="section">
+            <div className="container">
+              <div className="tile is-ancestor">
 
+                <div className="tile is-parent mx-6">
+                  <Link to="projects" className="tile is-child diagonal-box turtle-color-r">
+                    <div className="turtle-text has-text-centered">
+                      <p className="giext retro-font has-text-centered">Project</p>
+                    </div>
+                    <div className="turtle">
+                      <img src="assets/projects.png"></img>
+                    </div>
+                  </Link>
+                </div>
+
+                <div className="tile is-parent mx-6">
+                  <Link to="about" className="tile is-child diagonal-box turtle-color-y">
+                    <div className="turtle-text has-text-centered">
+                      <p className="giext retro-font has-text-centered">About zack</p>
+                    </div>
+                    <div className="turtle">
+                      <img src="assets/about.png"></img>
+                    </div>
+                  </Link>
+                </div>
+
+                <div className="tile is-parent mx-6">
+                  <Link to="gallery" className="tile is-child diagonal-box turtle-color-b">
+                    <div className="turtle-text has-text-centered">
+                      <p className="giext retro-font has-text-centered">Gallery</p>
+                    </div>
+                    <div className="turtle">
+                      <img src="assets/artgallery.png"></img>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+
+            </div>
           </div>
         </div>
       </div>
